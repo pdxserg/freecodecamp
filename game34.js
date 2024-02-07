3b5c89205fe0c53e8983a05bb98243f863b514fb
Sergey Starchenko <pdxserg@gmail.com>
2024-02-06 23:17:02 -0800

    game34

---
+++ game34.js
@@ -0,0 +1,16 @@
+// Step 34Passed
+// Finally, use querySelector() to get the #monsterHealth element. Because you have already declared a monsterHealth variable earlier, you need to use a different variable name for this element.
+
+// Declare a new variable with the const keyword and name it monsterHealthText.
+
+
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
